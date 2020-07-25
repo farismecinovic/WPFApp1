6d0fc048d1fa8b7b9c69e94a7a84a9d0f1496a6e
Faris Mecinovic <mecinovicfaris@hotmail.com>
2020-07-25 22:08:31 +0100

    Add project files.

---
+++ MojaWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MojaWPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Osoba> Osobe = new ObservableCollection<Osoba>();
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            DataContext = new Osoba();
+            BindingGroup = new BindingGroup();
+            datagrid.ItemsSource = Osobe;
+        }
+
+        private void Dodaj(object sender, RoutedEventArgs e)
+        {
+            Prozor p = new Prozor();
+            p.Owner = this;
+
+            if (p.ShowDialog().Value)
+            {
+                Osobe.Add(p.DataContext as Osoba);
+            }
+
+        }
+
+        private void Brisi(object sender, RoutedEventArgs e)
+        {
+            Osobe.Remove(datagrid.SelectedItem as Osoba);
+        }
+
+        private void Izmena(object sender, RoutedEventArgs e)
+        {
+            if (datagrid.SelectedItem != null)
+            {
+                Prozor p = new Prozor();
+                p.Owner = this;
+                p.DataContext = datagrid.SelectedItem;
+                p.ShowDialog();
+            }
+        }
+    }
+}
---
+++ MojaWPFApp/Prozor.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MojaWPFApp
+{
+    /// <summary>
+    /// Interaction logic for Prozor.xaml
+    /// </summary>
+    public partial class Prozor : Window
+    {
+ 
+        public Prozor()
+        {
+            InitializeComponent();
+            DataContext = new Osoba();
+            BindingGroup = new BindingGroup();
+           
+        }
+
+        private void Dodaj(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            BindingGroup.CommitEdit();
+            Close();
+        }
+
+        private void Odustani(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Izmeni(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
